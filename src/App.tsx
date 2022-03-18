6e66e4078572025b96fece4e91030579479d1172
Eralia <eralia.katsakiori@outlook.com>
2022-03-18 13:57:48 +0000

    List all baby names

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import babyNamesData from './data/babyNamesData.json'
+
+interface BabyNameInfo{
+    id: number;
+    name: string;
+    sex: string
+
+}
+
 export default function App(): JSX.Element {
+    function takeOneBabyName(babyName: BabyNameInfo) {
+        return <div key={babyName.id}> {babyName.name} </div>
+    }
   return (
     <>
       <h1>Baby Names</h1>
+      <h2>Here's a list of Baby Names to choose from:</h2>
+        <div>
+            {babyNamesData.map(takeOneBabyName)} 
+        </div>
     </>
   );
 }
