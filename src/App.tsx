2a90d505270ead82f465ba78c6d9d6c6d67dcd85
Eralia <eralia.katsakiori@outlook.com>
2022-03-18 17:11:16 +0000

    run prettier and lint + display baby names w flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import babyNamesData from './data/babyNamesData.json'
-import './App.css';
+import babyNamesData from "./data/babyNamesData.json";
+import "./App.css";
 
-interface BabyNameInfo{
-    id: number;
-    name: string;
-    sex: string
-
+interface BabyNameInfo {
+  id: number;
+  name: string;
+  sex: string;
 }
 
 export default function App(): JSX.Element {
-    function takeOneBabyName(babyName: BabyNameInfo) {
-        return <div className= {"BabyName " + babyName.sex} key={babyName.id}> {babyName.name} </div>
-    }
+  function takeOneBabyName(babyName: BabyNameInfo) {
+    return (
+      <div className={"BabyName " + babyName.sex} key={babyName.id}>
+        {" "}
+        {babyName.name}{" "}
+      </div>
+    );
+  }
+
+  function sortBabyNames() {}
+
   return (
     <>
-    <div className="App">
-      <h1>Baby Names</h1>
-      <h3>Here's a list of Baby Names to choose from:</h3>
-        <div> 
-            {babyNamesData.map(takeOneBabyName)} 
-        </div>
-    </div>
+      <div className="App">
+        <h1>Baby Names</h1>
+        <h3>Here's a list of Baby Names to choose from:</h3>
+        <div>{babyNamesData.map(takeOneBabyName)}</div>
+      </div>
     </>
   );
 }
