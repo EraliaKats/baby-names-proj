11d4c77d89d982978f82d6639e7f8ca3ea9e1758
Eralia Katsakiori <98091675+EraliaKats@users.noreply.github.com>
2022-03-21 17:41:57 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,3 +29,79 @@ export default function App(): JSX.Element {
     </>
   );
 }
+
+// import babyNamesData from "./data/babyNamesData.json";
+// import "./App.css";
+// import {useState} from "react"
+
+// interface BabyNameInfo {
+//   id: number;
+//   name: string;
+//   sex: string;
+// }
+
+// export default function App(): JSX.Element {
+//   function takeOneBabyName(babyName: BabyNameInfo) {
+//     return (
+//       <div className={"BabyName " + babyName.sex} key={babyName.id}>
+//         {" "}
+//         {babyName.name}{" "}
+//       </div>
+//     );
+//   }
+
+//   function compareBabyNames(
+//     babyNameInfoA: BabyNameInfo,
+//     babyNameInfoB: BabyNameInfo
+//   ) {
+//     if (babyNameInfoA.name < babyNameInfoB.name) {
+//       return -1;
+//     } else if (babyNameInfoA.name > babyNameInfoB.name) {
+//       return 1;
+//     } else {
+//       return 0;
+//     }
+//   }
+
+//   const sortedByNameBabyNames: BabyNameInfo[] = [...babyNamesData];
+//   sortedByNameBabyNames.sort(compareBabyNames);
+
+//   const [searchTerm, setSearchTerm] = useState("")
+//     function handleChangeInSearchBar(e:any){
+//       setSearchTerm(e.target.value)
+//     }
+
+//   function filterBabyNames(babyName: BabyNameInfo): boolean{
+//     const regPattern = RegExp (searchTerm, "i");
+//     if (searchTerm.match(regPattern)!== null){
+//       return true
+//     } else{
+//       return false;
+//     }
+//     // if (babyName.toString().toLowerCase().includes(searchTerm.toLowerCase())){
+//     //   return true
+//     // } else {
+//     //   return false
+//     // }
+//   }
+
+//   const filteredBabyNames = sortedByNameBabyNames.filter(filterBabyNames)
+
+
+//   return (
+//     <>
+//       <div className="App">
+//         <h1>Baby Names</h1>
+//         <input 
+//           placeholder="Search for... "
+//           value={searchTerm}
+//           onChange={handleChangeInSearchBar}
+//         /> 
+//         <p> Currently searching for: {searchTerm} </p>
+//         <h4>Here's a list of Baby Names to choose from:</h4>
+//         <hr/>
+//         <div>{filteredBabyNames.map(takeOneBabyName)}</div>
+//       </div>
+//     </>
+//   );
+// }
